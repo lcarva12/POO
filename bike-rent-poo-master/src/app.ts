726df19d48b038e8a20c0ba86d2738bd65647198
Lucas Carvalho <lcarva12@hotmail.com>
2023-09-02 22:40:26 -0300

    atualizacao_codigo_aluguel_bike

---
+++ bike-rent-poo-master/src/app.ts
@@ -0,0 +1,24 @@
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
+import crypto from 'crypto'
+
+export class App {
+    users: User[] = []
+    bikes: Bike[] = []
+    rents: Rent[] = []
+
+    findUser(email: string): User {
+        return this.users.find(user => user.email === email)
+    }
+
+    registerUser(user: User): void {
+        for (const rUser of this.users) {
+            if (rUser.email === user.email) {
+                throw new Error('Duplicate user.')
+            }
+        }
+        user.id = crypto.randomUUID()
+        this.users.push(user)
+    }
+}
