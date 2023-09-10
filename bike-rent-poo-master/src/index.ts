c8df909019b68f20f8026345f62545d390b16250
Lucas Carvalho <lcarva12@hotmail.com>
2023-09-10 15:47:19 -0300

    atividade_4
    Listagens e senha salva criptografada, com verificação de login

--- bike-rent-poo-master/src/index.ts
+++ bike-rent-poo-master/src/index.ts
@@ -7,7 +7,7 @@ const app = new App()
 const bike = new Bike('caloi mountain', 'mountain bike', 100, 200, 150.5, 
     'My bike', 5, [])
 const bikeId = app.registerBike(bike)
-console.log(app.bikes)
+//console.log(app.bikes)
 const user1 = new User('Jose', 'jose@mail.com', '1234')
 const user2 = new User('Maria', 'maria@mail.com', '1234')
 app.registerUser(user1)
@@ -25,14 +25,32 @@ twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 3)
 
 app.rentBike(bikeId, 'jose@mail.com', yesterday, today)
 
-console.log('Antes do retorno', app.rents)
-
 app.returnBike(bikeId, 'jose@mail.com')
 
-console.log('Depois do retorno', app.rents)
+// Listagem de usuários cadastrados
+const allUsers = app.listUsers();
+console.log("Usuários cadastrados:", allUsers);
 
+// Listagem de reservas/aluguéis cadastrados
+const allRents = app.listRents();
+console.log("Reservas/Aluguéis cadastrados:", allRents);
 
+// Listagem de bikes cadastradas
+const allBikes = app.listBikes();
+console.log("Bikes cadastradas:", allBikes);
 
+// Teste de Login
+const userEmail = 'jose@mail.com';
+const userPassword = '1234';
+
+const verificacao = app.authenticateUser(userEmail, userPassword);
+console.log("acesso:", verificacao);
+
+if (app.authenticateUser(userEmail, userPassword)) {
+    console.log('Usuário autenticado com sucesso.');
+} else {
+    console.log('Autenticação falhou. Verifique o email e a senha.');
+}
 
 
 
