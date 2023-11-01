6344da3339b92e827e87bfd808c707b1bcb80ab1
Lucas Carvalho <lcarva12@hotmail.com>
2023-11-01 19:17:23 -0300

    atividade-13-rev-2

--- bike-rent-poo-master/src/errors/open-rents-error.ts
+++
@@ -1,7 +0,0 @@
-export class OpenRentsError extends Error {
-    constructor() {
-      super("Existem aluguéis em aberto para este usuário.");
-      this.name = "OpenRentsError";
-    }
-  }
-  
--- bike-rent-poo-master/src/errors/rent-error.ts
+++
@@ -1,7 +0,0 @@
-export class RentError extends Error {
-    public readonly name = 'RentError'
-
-    constructor() {
-        super('Rent not found.')
-    }
-}
